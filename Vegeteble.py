0bbbce3670ab8b78724c9d843ebaeea5c0eeb171
klimovstas093-create <klimovstas093@gmail.com>
2026-08-13 20:16:57 +0300

    Update Vegeteble.py

--- Vegeteble.py
+++ Vegeteble.py
@@ -1,16 +1,17 @@
 #код Манжесова Алексея
-class Vegeteble():
+class Vegetable():
     def __init__(self, name, water, nutrients, light):
         self.name = name
         self.water = water 
         self.nutrients = nutrients
         self.light = light
 
-    def print_info(self):
-        print('вид образца:', self.name)
-        print('Обеспеченность водой:', self.water)
-        print('Обеспеченность питательными веществами:', self.nutrients)
-        print('Обеспеченность светом:', self.light)
+    def __repr__(self):
+        return f'''
+Вид образца: {self.name}
+Обеспеченность водой: {self.water:.2f}
+Обеспеченность питательными веществами: {self.nutrients:.2f}
+Обеспеченность светом: {self.light:.2f}'''
 
     def check_state(self, required_water, required_nutrients, required_light):
         if (self.water >= required_water and self.nutrients >= required_nutrients and self.light >= required_light):
