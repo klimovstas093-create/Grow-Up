e046640f6aa2c45c6225a6367f15b3df6866a534
klimovstas093-create <klimovstas093@gmail.com>
2026-08-13 20:16:29 +0300

    Update Assistant.py

--- Assistant.py
+++ Assistant.py
@@ -1,26 +1,26 @@
-#код Бабошина максима
+#код Бабошина Максима
 class Assistant():
     def __init__(self, name, position):
         self.name = name
         self.position = position
 
-    def print_info(self):
-        print('Лаборант:', self.name, 'Должность:', self.position)
+    def __repr__(self):
+        return 'Лаборант:', self.name, 'Должность:', self.position
 
     def water_sample(self, sample): 
         sample.water += 1
         print(self.name,'полил образец, обеспеченность водой образца:', sample.water)
 
     def udobrenie_sample(self, sample):
-        sample.nutrient += 1
-        print(self.name, 'внес удобрение на образец, обеспеченность питательными веществами образца:', sample.nutrient)
+        sample.nutrients += 1
+        print(self.name, 'внес удобрение на образец, обеспеченность питательными веществами образца:', sample.nutrients)
 
     def light_sample(self, sample):
         sample.light += 1
         print(self.name, 'включил дополнительное освещение для образца, обеспеченность светом образца:',  sample.light)
 
-    def check_certificate(self, sample):
-        if sample.water > 0 and sample.nutrient > 0 and sample.light > 0:
-            print('Прошёл аттестацию и допущен до работы с настоящими опытными образцами.')
-        elif sample.water <= 0 and sample.nutrient <= 0 and sample.light <= 0:
-            print('Не прошёл аттестацию. Отправляется на пересдачу.')
+    def check_certificate(self, samples_states):
+        if False in samples_states:
+            print(self.name, 'не прошёл аттестацию. Отправляется на пересдачу.')
+        else:
+            print(self.name, 'прошёл аттестацию и допущен до работы с настоящими опытными образцами.')
